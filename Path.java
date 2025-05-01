94aee4fa52d8a837d45a0232f0603bcb502c9549
dakrow11 <drrowland@cop.edu>
2025-05-01 10:26:37 -0700

    Moved more java files

--- Path.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class Path {
-	private List<Direction> moves;
-	private int movementCost;
-	private int waterCost;
-	private int foodCost;
-}
---
+++ src/main/java/wss/game/Path.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Path {
+	private List<Direction> moves;
+	private int movementCost;
+	private int waterCost;
+	private int foodCost;
+}
