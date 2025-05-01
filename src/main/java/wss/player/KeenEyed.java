f7b44d801514ef78ef1bcbe70e493d8568449216
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 14:56:37 -0700

    Update KeenEyed.java

--- src/main/java/wss/player/KeenEyed.java
+++ src/main/java/wss/player/KeenEyed.java
@@ -1,5 +1,10 @@
 package wss.player;
 
+import wss.player.Player;
+import wss.game.Path;
+import wss.game.Map;
+
+
 public class KeenEyed extends Vision {
 	@Override
 	public Path closestFood(Player player, Map map) {
