8175583ad817691cfd94eabd7cbd37ba5c7bba4b
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 14:45:56 -0700

    Update Main.java

--- src/main/java/wss/Main.java
+++ src/main/java/wss/Main.java
@@ -3,5 +3,11 @@ package wss;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+
+		//just a dummy code for testing
+		Player p = new Player(10, 10, 10, new Cautious(), new ConservativeBrain());
+		System.out.println("Position: " + p.getX() + ", " + p.getY());
+		p.rest();
+
 	}
 }
