1592162efaff5427ab1de0b8398f987df2accad8
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 14:48:02 -0700

    Update Square.java

--- src/main/java/wss/game/Square.java
+++ src/main/java/wss/game/Square.java
@@ -10,5 +10,14 @@ public class Square {
 
 		public void enterPlayer(Player p) {  }
 		public void collectItems(Player p) {  }
+
+		public void setItem(Item item) {
+    			this.item = item;
+		}
+	
+		public boolean hasFoodBonus() {
+    			return item instanceof FoodBonus;
+		}
+
 }
 
