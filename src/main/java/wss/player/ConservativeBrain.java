069eab70d522a5961cb2dca49836c995768c4f50
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 15:07:29 -0700

    Update ConservativeBrain.java

--- src/main/java/wss/player/ConservativeBrain.java
+++ src/main/java/wss/player/ConservativeBrain.java
@@ -5,8 +5,24 @@ import wss.game.Map;
 
 
 public class ConservativeBrain extends Brain {
-	@Override
-	public void makeMove(Player player, Map map) {
+@Override
+public void makeMove(Player player, Map map) {
+    Path path = player.getVision().closestFood(player, map);
 
-	}
+    if (path == null) {
+        System.out.println("No path to food found.");
+        return;
+    }
+
+    if (path.getSteps().isEmpty()) {
+        System.out.println("Path is empty.");
+        return;
+    }
+
+    Direction dir = path.getSteps().get(0);
+    boolean success = player.move(dir, map.getGrid());
+
+    System.out.println("Tried to move " + dir + ": " + (success ? "Success" : "Failed"));
+}
+
 }
