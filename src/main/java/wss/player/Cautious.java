f57f6d618df81a756194d786d70cee483e1a1d87
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 15:01:40 -0700

    Update Cautious.java

--- src/main/java/wss/player/Cautious.java
+++ src/main/java/wss/player/Cautious.java
@@ -7,10 +7,19 @@ import wss.player.Player;
 
 
 public class Cautious extends Vision {
-	@Override
-	public Path closestFood(Player player, Map map) {
-		return null;
-	}
+@Override
+public Path closestFood(Player player, Map map) {
+    int x = player.getX();
+    int y = player.getY();
+    Square squareEast = map.getSquare(x + 1, y);
+
+    if (map.inBounds(x + 1, y) && squareEast.hasFoodBonus()) {
+        return Path.of(Direction.EAST, squareEast.getMovementCost(), squareEast.getWaterCost(), squareEast.getFoodCost());
+    }
+
+    return null;
+}
+
 
 	@Override
 	public Path closestWater(Player player, Map map) {
