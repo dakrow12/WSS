a356dc4e74b930efbedda43a63424d5830f405f3
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 14:48:39 -0700

    Update Map.java

--- src/main/java/wss/game/Map.java
+++ src/main/java/wss/game/Map.java
@@ -1,12 +1,17 @@
 package wss.game;
 
 public class Map {
-		private int width, height;
-		private Square[][] grid;
+    private Square[][] grid;
 
-		public Map(int width, int height) { }
+    public Map(Square[][] grid) {
+        this.grid = grid;
+    }
 
-		public void genTerrain(Difficulty diff) { }
+    public boolean inBounds(int x, int y) {
+        return x >= 0 && y >= 0 && x < grid.length && y < grid[0].length;
+    }
 
-		public void createMap(int width, int height) { }
+    public Square getSquare(int x, int y) {
+        return grid[x][y];
+    }
 }
