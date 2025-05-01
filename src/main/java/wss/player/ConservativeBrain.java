2f0b81de6ede5a315a9effb668d73ca7f6d597c1
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 15:16:27 -0700

    Update ConservativeBrain.java

--- src/main/java/wss/player/ConservativeBrain.java
+++ src/main/java/wss/player/ConservativeBrain.java
@@ -13,19 +13,22 @@ public void makeMove(Player player, Map map) {
     Path path = player.getVision().closestFood(player, map);
 
     if (path == null) {
-        System.out.println("No path to food found.");
-        return;
+        path = player.getVision().closestWater(player, map);
     }
 
-    if (path.getSteps().isEmpty()) {
-        System.out.println("Path is empty.");
-        return;
+    if (path == null) {
+        path = player.getVision().easiestPath(player, map);
     }
 
-    Direction dir = path.getSteps().get(0);
-    boolean success = player.move(dir, map.getGrid());
-
-    System.out.println("Tried to move " + dir + ": " + (success ? "Success" : "Failed"));
+    if (path != null && !path.getSteps().isEmpty()) {
+        Direction dir = path.getSteps().get(0);
+        boolean moved = player.move(dir, map.getGrid());
+        System.out.println("Tried to move " + dir + ": " + (moved ? "Success" : "Failed"));
+    } else {
+        System.out.println("No path found. Resting...");
+        player.rest();
+    }
 }
+
 
 }
