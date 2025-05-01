2e4c63918cf335db73b47299bed8a04a1049e4a1
dakrow12 <dakotarowland03@gmail.com>
2025-05-01 14:57:29 -0700

    Update ConservativeBrain.java

--- src/main/java/wss/player/ConservativeBrain.java
+++ src/main/java/wss/player/ConservativeBrain.java
@@ -1,5 +1,9 @@
 package wss.player;
 
+import wss.player.Player;
+import wss.game.Map;
+
+
 public class ConservativeBrain extends Brain {
 	@Override
 	public void makeMove(Player player, Map map) {
