2d0b617eee93ba43d51969e17a75ae84b283f016
hhnguyen9581 <89948043+hhnguyen9581@users.noreply.github.com>
2025-05-04 21:04:35 -0700

    Update GoldBonus.java

--- src/main/java/wss/items/GoldBonus.java
+++ src/main/java/wss/items/GoldBonus.java
@@ -7,11 +7,21 @@ public class GoldBonus extends Item {
 
 	private int goldAmount;
 
+	/**
+	 * Calls the superclass constructor
+	 * @param amount The amount of gold bonus given to player.
+  	 * @param repeating Check if the item can be used multiple times.
+    */
 	public GoldBonus(int amount, boolean repeating)
 	{
 		super(repeating);
 		this.goldAmount = amount;
 	}
+
+	/**
+	 * Calls the superclass constructor
+	 * @param amount The amount of gold bonus given to player.
+    */
 	public void activate(Player player) {
 		player.addGold(goldAmount);
 	}
