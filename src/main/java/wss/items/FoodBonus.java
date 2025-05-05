268ec35b2b4dc1e6626e628c051cbce0b26a93ae
hhnguyen9581 <89948043+hhnguyen9581@users.noreply.github.com>
2025-05-04 21:04:01 -0700

    Update FoodBonus.java

--- src/main/java/wss/items/FoodBonus.java
+++ src/main/java/wss/items/FoodBonus.java
@@ -5,6 +5,12 @@ import wss.player.Player;
 public class FoodBonus extends Item {
     private int foodAmount;
 
+
+    /**
+	 * Calls the superclass constructor
+	 * @param amount The amount of food bonus given to player.
+  	 * @param repeating Check if the item can be used multiple times.
+    */
     public FoodBonus(int amount, boolean repeating) {
         super(repeating);
         this.foodAmount = amount;
@@ -15,7 +21,7 @@ public class FoodBonus extends Item {
     /**
 	 * Activates the item's effect of giving food to the player.
 	 * @param player The player activating the item.
-	 */
+    */
     public void activate(Player player) {
         player.addFood(foodAmount);
     }
